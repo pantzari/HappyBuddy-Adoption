ff79570310bf9899351750b35ba65b9924eb5d9e
pantzari <ferousikostantina@gmail.com>
2025-07-15 13:51:12 +0300

    Added the div ids for smooth scrolling to sections and the alert box when clicking the buttons

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelectorAll('.dog-bttn, .more-bttn-txt').forEach(button => button.addEventListener('click', () => {
+    alert('This is a Demo site to display responsive design and web design skills!');
+}));
